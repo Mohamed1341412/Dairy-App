8b5e3cac7c67480d5d107432d47a9a4dcf57346d
Mohamed <mido.bakr555@gmail.com>
2026-07-09 04:40:23 +0300

    Create 1783560220_updated_production_outputs.js
    Update schema to Add unique index to production_outputs

---
+++ backend/pocketbase/pb_migrations/1783560220_updated_production_outputs.js
@@ -0,0 +1,21 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_1934478955")
+
+  // update collection data
+  unmarshal({
+    "indexes": [
+      "CREATE UNIQUE INDEX `idx_production_outputs_unique` ON `production_outputs` (`production_batch_id`, `product_id`)"
+    ]
+  }, collection)
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_1934478955")
+
+  // update collection data
+  unmarshal({
+    "indexes": []
+  }, collection)
+
+})
