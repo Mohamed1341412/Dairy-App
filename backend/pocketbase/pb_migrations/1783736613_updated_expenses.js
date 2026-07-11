72da4d9b3386b6baa01792c19b64720ee3fe3d8a
Mohamed <mido.bakr555@gmail.com>
2026-07-11 05:27:09 +0300

    Create 1783736613_updated_expenses.js
    add :
    expense.status : [draft, approved, cancelled]
    and
    expene.reference_number

---
+++ backend/pocketbase/pb_migrations/1783736613_updated_expenses.js
@@ -0,0 +1,51 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_1691921218")
+
+  // add field
+  collection.fields.addAt(7, new Field({
+    "help": "",
+    "hidden": false,
+    "id": "select2063623452",
+    "maxSelect": 0,
+    "name": "status",
+    "presentable": false,
+    "required": false,
+    "system": false,
+    "type": "select",
+    "values": [
+      "draft",
+      "approved",
+      "cancelled"
+    ]
+  }))
+
+  // add field
+  collection.fields.addAt(8, new Field({
+    "autogeneratePattern": "",
+    "help": "",
+    "hidden": false,
+    "id": "text2347871824",
+    "max": 0,
+    "min": 0,
+    "name": "reference_number",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_1691921218")
+
+  // remove field
+  collection.fields.removeById("select2063623452")
+
+  // remove field
+  collection.fields.removeById("text2347871824")
+
+  return app.save(collection)
+})
